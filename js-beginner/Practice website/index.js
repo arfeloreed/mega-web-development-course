edeaf032e6848048209a763bf0aaa80f89c2e9ee
Arfelo Reed <116253120+mr-fravardin@users.noreply.github.com>
2023-07-15 13:22:28 +0800

    Add files via upload

---
+++ js-beginner/Practice website/index.js
@@ -0,0 +1,44 @@
+//  If statement
+
+// var isWorking = false;
+// var isCarReady = false;
+
+// if (isWorking) {
+//     console.log("I have work to do.")
+// }
+// if (!isWorking) {
+//     if (isCarReady) {
+//         console.log("I wanna Party party!!")
+//     } else {
+//         console.log("I can't go. My car isn't fixed yet.")
+//     }
+// }
+
+
+//  function
+
+// function dailyBattle() {
+//     var moneySave = made - spent
+//     console.log("Today I made $" + made);
+//     console.log("Today I spent $" + spent);
+//     if (moneySave >= 1) {
+//         console.log("Total money save for today is $" + moneySave);
+//         console.log("I won today's battle.");
+//     } else {
+//         moneySave *= -1
+//         console.log("Total money save for today is -$" + moneySave);
+//         console.log("I lose today's battle.");
+//     }    
+// }
+
+// var made = 10;
+// var spent = 5;
+
+// dailyBattle()
+
+
+// on click
+
+function becomeMember() {
+    console.log("Welcome to the club!!")
+}
